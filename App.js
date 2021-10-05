ead8050f13602150e0fd2c0df8a1cbcb83d2be73
Brian Ridolce <brianridolce@gmail.com>
2021-10-05 22:22:46 +0200

    feat: App.js text review

--- App.js
+++ App.js
@@ -1,21 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import tw from 'tailwind-react-native-classnames'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={tw`my-auto mx-auto`}>
+      <Text style={tw`text-xl text-blue-500`}>Template Minimum pour lancer une App React Native</Text>
+      <Text style={tw`text-xl text-blue-500 text-center`}>Avec une config Tailwind CSS</Text>
+      <Text style={tw`text-xl text-blue-500 text-center`}>Node Sass pour gérer les fichiers styles scss</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
